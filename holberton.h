c772d4fc3afaea529b4e1fde24d7dbaadc81221a
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-15 18:25:10 +0000

    adding the putchar and strlen funcions

--- holberton.h
+++ holberton.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+int _putchar(char c);
+int _strlen(char *s);
 int _printf(const char *format, ...);
 /**
  * struct fmt - function to check for formats
